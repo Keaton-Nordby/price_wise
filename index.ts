60960b0b4e5ae3aeecc7b36538a230c28e90b5fc
Keaton Nordby <keatonnordby4@gmail.com>
2024-12-19 12:09:13 -0600

    Add files via upload
    JSON / types commit

--- index.ts
+++ index.ts
@@ -1,85 +1,44 @@
-"use server"
+export type PriceHistoryItem = {
+  price: number;
+};
 
-import axios from 'axios';
-import * as cheerio from 'cheerio';
-import { extractCurrency, extractDescription, extractPrice } from '../utils';
+export type User = {
+  email: string;
+};
 
-export async function scrapeAmazonProduct(url: string) {
-  if(!url) return;
+export type Product = {
+  _id?: string;
+  url: string;
+  currency: string;
+  image: string;
+  title: string;
+  currentPrice: number;
+  originalPrice: number;
+  priceHistory: PriceHistoryItem[] | [];
+  highestPrice: number;
+  lowestPrice: number;
+  averagePrice: number;
+  discountRate: number;
+  description: string;
+  category: string;
+  reviewsCount: number;
+  stars: number;
+  isOutOfStock: Boolean;
+  users?: User[];
+};
 
-  // BrightData proxy configuration
-  const username = String(process.env.BRIGHT_DATA_USERNAME);
-  const password = String(process.env.BRIGHT_DATA_PASSWORD);
-  const port = 22225;
-  const session_id = (1000000 * Math.random()) | 0;
+export type NotificationType =
+  | "WELCOME"
+  | "CHANGE_OF_STOCK"
+  | "LOWEST_PRICE"
+  | "THRESHOLD_MET";
 
-  const options = {
-    auth: {
-      username: `${username}-session-${session_id}`,
-      password,
-    },
-    host: 'brd.superproxy.io',
-    port,
-    rejectUnauthorized: false,
-  }
+export type EmailContent = {
+  subject: string;
+  body: string;
+};
 
-  try {
-    // Fetch the product page
-    const response = await axios.get(url, options);
-    const $ = cheerio.load(response.data);
-
-    // Extract the product title
-    const title = $('#productTitle').text().trim();
-    const currentPrice = extractPrice(
-      $('.priceToPay span.a-price-whole'),
-      $('.a.size.base.a-color-price'),
-      $('.a-button-selected .a-color-base'),
-    );
-
-    const originalPrice = extractPrice(
-      $('#priceblock_ourprice'),
-      $('.a-price.a-text-price span.a-offscreen'),
-      $('#listPrice'),
-      $('#priceblock_dealprice'),
-      $('.a-size-base.a-color-price')
-    );
-
-    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
-
-    const images = 
-      $('#imgBlkFront').attr('data-a-dynamic-image') || 
-      $('#landingImage').attr('data-a-dynamic-image') ||
-      '{}'
-
-    const imageUrls = Object.keys(JSON.parse(images));
-
-    const currency = extractCurrency($('.a-price-symbol'))
-    const discountRate = $('.savingsPercentage').text().replace(/[-%]/g, "");
-
-    const description = extractDescription($)
-
-    // Construct data object with scraped information
-    const data = {
-      url,
-      currency: currency || '$',
-      image: imageUrls[0],
-      title,
-      currentPrice: Number(currentPrice) || Number(originalPrice),
-      originalPrice: Number(originalPrice) || Number(currentPrice),
-      priceHistory: [],
-      discountRate: Number(discountRate),
-      category: 'category',
-      reviewsCount:100,
-      stars: 4.5,
-      isOutOfStock: outOfStock,
-      description,
-      lowestPrice: Number(currentPrice) || Number(originalPrice),
-      highestPrice: Number(originalPrice) || Number(currentPrice),
-      averagePrice: Number(currentPrice) || Number(originalPrice),
-    }
-
-    return data;
-  } catch (error: any) {
-    console.log(error);
-  }
-}
+export type EmailProductInfo = {
+  title: string;
+  url: string;
+};
